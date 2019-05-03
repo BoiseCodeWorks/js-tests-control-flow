92c829340d193655c817b61511a61a9c82885eef
Catherine Buergel <catherinebuergel@gmail.com>
2019-05-03 15:56:02 -0600

    mid time function

--- app/conditionals.js
+++ app/conditionals.js
@@ -16,4 +16,13 @@ function testGrader(score, possible) {
 // --------------------------------------------
 
 
-// 2. Given a new function thing
+// 2. Given an integer that represents an hour in the day (1 - 24), write a function that returns "good morning", "good afternoon", "good evening", or "good night" based off of what hour it is. You'll need to think in military time.
+//Morning is between 5am and 11am (5 - 11)
+//afternoon is between 12pm and 5pm (12 - 17)
+//evening is between 6pm and 9pm (18 - 21)
+//night is between 10pm and 4am (22 -24 and 1 - 4)
+//Make sure your ranges are inclusive
+
+function timeOfDayGreeting(hour) {
+
+}
--- test/modules/conditionals.test.js
+++ test/modules/conditionals.test.js
@@ -7,7 +7,25 @@ describe("conditionals.js", () => {
          chai.assert.strictEqual(testGrader(23, 25).toUpperCase(), "A", "Return can only be either 'A', 'B', 'C', 'D', 'F'.")
       })
       it("letter grade is case sensitive", () => {
-        chai.assert.strictEqual(testGrader(19, 25), "C", "Letter grade must be capitalized.")
+         chai.assert.strictEqual(testGrader(19, 25), "C", "Letter grade must be capitalized.")
       })
+   })
+   describe("timeOfDayGreeting", () => {
+      it("Function should return a string", () => {
+         chai.assert.isString(timeOfDayGreeting(5), "Be sure to return a greeting")
+      })
+      it("Function should return Good Morning during the morning hours", () => {
+         chai.assert.isTrue(timeOfDayGreeting(5) == "good morning" && timeOfDayGreeting(11) == "good morning" && timeOfDayGreeting(7) == "good morning", "Be sure to return 'good morning' between (and including) the hours 5 and 11")
+      })
+      it("Function should return Good Afternoon during afternoon hours", () => {
+         chai.assert.isTrue(timeOfDayGreeting(13) == "good afternoon" && timeOfDayGreeting(12) == "good afternoon" && timeOfDayGreeting(17) == "good afternoon", "Be sure to return 'good afternoon' between (and including) the hours 12 and 17")
+      })
+      it("Function should return Good Evening for evening hours", () => {
+         chai.assert.isTrue(timeOfDayGreeting(18) == "good evening" && timeOfDayGreeting(20) == "good evening" && timeOfDayGreeting(21) == "good evening", "Be sure to return 'good evening' between the hours of 22 - 24 and 1 - 4")
+      })
+      it("Function should return Good Night for evening hours", () => {
+         chai.assert.isTrue(timeOfDayGreeting(22) == "good night" && timeOfDayGreeting(23) == "good night" && timeOfDayGreeting(24) == "good night", "Be sure to return 'good night' between (and including) the hours 22 and 24")
+      })
+
    })
 })
