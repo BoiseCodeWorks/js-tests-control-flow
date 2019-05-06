7505c23700693bbb2609fb63413e01322d192d11
Catherine Buergel <catherinebuergel@gmail.com>
2019-05-06 11:07:16 -0600

    6/7 conditionals complete

--- app/conditionals.js
+++ app/conditionals.js
@@ -1,4 +1,10 @@
-// 1. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
+//1. Given two numbers, write a function that will return  the larger number
+
+function largerNum(num1, num2) {
+
+}
+
+// 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
 // Example: 
 // input: 23, 25
 // output: "A"
@@ -16,7 +22,7 @@ function testGrader(score, possible) {
 // --------------------------------------------
 
 
-// 2. Given an integer that represents an hour in the day (1 - 24), write a function that returns "good morning", "good afternoon", "good evening", or "good night" based off of what hour it is. You'll need to think in military time.
+// 3. Given an integer that represents an hour in the day (1 - 24), write a function that returns "good morning", "good afternoon", "good evening", or "good night" based off of what hour it is. You'll need to think in military time.
 //Morning is between 5am and 11am (5 - 11)
 //afternoon is between 12pm and 5pm (12 - 17)
 //evening is between 6pm and 9pm (18 - 21)
@@ -24,5 +30,76 @@ function testGrader(score, possible) {
 //Make sure your ranges are inclusive
 
 function timeOfDayGreeting(hour) {
+
+}
+
+//4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
+function fever(temp) {
+
+}
+
+//5. Write a function that takes in a car object, if it is not moving then return true
+let myCar = {
+    make: "Ford",
+    model: "Mustang",
+    color: "Red",
+    moving: false
+}
+
+function isStopped(car) {
+
+}
+
+//6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
+
+let dish = {
+    yourDish: true,
+    isDirty: true
+}
+
+function washDish(dish) {
+
+}
+
+//7. Flights from Boise. Write a function that will use the following data to return the cost of flights from boise to a neighboring city, by default return the standard cost unless firstClass is set to true
+
+let flights = [{
+    from: 'BOI',
+    to: 'LAX',
+    prices: {
+        standard: 500,
+        firstClass: 2200
+    }
+}, {
+    from: 'BOI',
+    to: 'SEA',
+    prices: {
+        standard: 800,
+        firstClass: 1200
+    }
+}, {
+    from: 'BOI',
+    to: 'CAN',
+    prices: {
+        standard: 750,
+        firstClass: 6200
+    }
+}]
+
+
+function flightCost(destination, firstClass) {
+    //***hint: use the find method***
+    let flight = flights.find(i => i.to == destination)
+    if (firstClass) {
+        return flight.prices["firstClass"]
+    } else {
+        return flight.prices["standard"]
+    }
+}
+
+// 7. Given a number 1-7 return the corresponding day of the week.
+// allow a secondary argument (bool) to change the first day of the week to monday
+
+function getDayOfWeek(n, offsetForMon) {
 
 }
--- test/modules/conditionals.test.js
+++ test/modules/conditionals.test.js
@@ -1,4 +1,11 @@
 describe("conditionals.js", () => {
+   describe("largerNum Function", () => {
+      it("Returns the larger number", () => {
+         chai.assert.strictEqual(largerNum(10, 4), 10, "Be sure to return the larger of the two numbers")
+         chai.assert.strictEqual(largerNum(18, 20), 20, "Be sure to return the larger of the two numbeers")
+
+      })
+   })
    describe("testGrader Function", () => {
       it("returns a string", () => {
          chai.assert.isString(testGrader(23, 25), "Return type must be a string.")
@@ -15,17 +22,91 @@ describe("conditionals.js", () => {
          chai.assert.isString(timeOfDayGreeting(5), "Be sure to return a greeting")
       })
       it("Function should return Good Morning during the morning hours", () => {
-         chai.assert.isTrue(timeOfDayGreeting(5) == "good morning" && timeOfDayGreeting(11) == "good morning" && timeOfDayGreeting(7) == "good morning", "Be sure to return 'good morning' between (and including) the hours 5 and 11")
+         chai.assert.isTrue(timeOfDayGreeting(5) == "Good Morning" && timeOfDayGreeting(11) == "Good Morning" && timeOfDayGreeting(7) == "Good Morning", "Be sure to return 'Good Morning' between (and including) the hours 5 and 11")
       })
       it("Function should return Good Afternoon during afternoon hours", () => {
-         chai.assert.isTrue(timeOfDayGreeting(13) == "good afternoon" && timeOfDayGreeting(12) == "good afternoon" && timeOfDayGreeting(17) == "good afternoon", "Be sure to return 'good afternoon' between (and including) the hours 12 and 17")
+         chai.assert.isTrue(timeOfDayGreeting(13) == "Good Afternoon" && timeOfDayGreeting(12) == "Good Afternoon" && timeOfDayGreeting(17) == "Good Afternoon", "Be sure to return 'Good Afternoon' between (and including) the hours 12 and 17")
       })
       it("Function should return Good Evening for evening hours", () => {
-         chai.assert.isTrue(timeOfDayGreeting(18) == "good evening" && timeOfDayGreeting(20) == "good evening" && timeOfDayGreeting(21) == "good evening", "Be sure to return 'good evening' between the hours of 22 - 24 and 1 - 4")
+         chai.assert.isTrue(timeOfDayGreeting(18) == "Good Evening" && timeOfDayGreeting(20) == "Good Evening" && timeOfDayGreeting(21) == "Good Evening", "Be sure to return 'Good Evening' between the hours of 22 - 24 and 1 - 4")
       })
-      it("Function should return Good Night for evening hours", () => {
-         chai.assert.isTrue(timeOfDayGreeting(22) == "good night" && timeOfDayGreeting(23) == "good night" && timeOfDayGreeting(24) == "good night", "Be sure to return 'good night' between (and including) the hours 22 and 24")
+      it("Function should return Good Night for nightime hours between 22 and 24", () => {
+         chai.assert.isTrue(timeOfDayGreeting(22) == "Good Night" && timeOfDayGreeting(23) == "Good Night" && timeOfDayGreeting(24) == "Good Night", "Be sure to return 'Good Night' between (and including) the hours 22 and 24")
+      })
+      it("Function should return Good Night for nighttime hours between 1 and 4", () => {
+         chai.assert.isTrue(timeOfDayGreeting(1) == "Good Night" && timeOfDayGreeting(3) == "Good Night" && timeOfDayGreeting(4) == "Good Night", "Be sure to return 'Good Night' between (and including) the hours 1 and 4")
       })
 
    })
+   describe("fever Function", () => {
+      it("Should return a string", () => {
+         chai.assert.isString(fever(98), "Be sure to return a string")
+      })
+      it("Should return 'no fever' if temp is under or equal to 98.6", () => {
+         chai.assert.isTrue(fever(98) == "no fever", "Be sure to return 'no fever' is temp is below 98.6")
+         chai.assert.isTrue(fever(75) == "no fever")
+         chai.assert.isTrue(fever(98.6) == "no fever", "Be sure to return 'no fever' if temp is equal to 98.6")
+      })
+      it("Should return 'fever' if temp is above 98.6", () => {
+         chai.assert.isTrue(fever(98.7) == "fever", "Be sure to return 'fever' if the temp is above 98.6")
+         chai.assert.isTrue(fever(101) == "fever", "Be sure to return 'fever' if temp is above 98.6")
+      })
+      it("Should return 'fever go to hospital'", () => {
+         chai.assert.isTrue(fever(103) == "fever go to hospital", "Be sure to return 'fever go to hospital' if temp is at or above 103")
+         chai.assert.isTrue(fever(110) == 'fever go to hospital', "Be sure to return 'fever go to hospital' if temp is at or above 103")
+      })
+   })
+   describe("isStopped Function", () => {
+      let movingCar = {
+         moving: true
+      }
+      let stoppedCar = {
+         moving: false
+      }
+      it("Should return a boolean value", () => {
+         chai.assert.isBoolean(isStopped(movingCar), "Be sure to return a boolean value")
+      })
+      it("Should return false if the car object is  moving", () => {
+         chai.assert.strictEqual(isStopped(movingCar), false, "Be sure to return false if the car is moving")
+      })
+      it("Should return true if the car is not moving", () => {
+         chai.assert.strictEqual(isStopped(stoppedCar), true, "Be sure to return true if the car is stopped")
+      })
+   })
+   describe("washDish Function", () => {
+      let dirtyDish = {
+         yourDish: false,
+         isDirty: true,
+      }
+      let myDish = {
+         yourDish: true,
+         isDirty: false,
+      }
+      let myDirtyDish = {
+         yourDish: true,
+         isDirty: true
+      }
+      it("Should return false if the one of the properties is false", () => {
+         chai.assert.isTrue(washDish(dirtyDish) == false && washDish(myDish) == false, "Be sure to return false if the dish is not yours or it is not dirty")
+      })
+      it("Should return true if the dish is yours and it is dirty", () => {
+         chai.assert.strictEqual(washDish(myDirtyDish), true, "Be sure to return true if the dish is yours and it is dirty")
+      })
+   })
+   describe("flightCost Function", () => {
+      let flightArray
+      try {
+         flightArray = flights
+      } catch (e) { console.error(e) }
+      it("Should return the standard price of the flight if the firstClass parameter is set to false", () => {
+         chai.assert.strictEqual(flightCost("LAX", false), 500, "Be sure to return the standard cost if flightClass is set to false")
+         chai.assert.strictEqual(flightCost("CAN", false), 750)
+      })
+
+      it("Should return the firstclass price of a flight if firstClass is set to true", () => {
+         chai.assert.strictEqual(flightCost("SEA", true), 1200, "Be sure to check if firstClass is set to true")
+         chai.assert.strictEqual(flightCost("CAN", true), 6200)
+      })
+   })
 })
+
